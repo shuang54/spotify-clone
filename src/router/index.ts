05bf011e99150c2df5755a29556e8b35364b2868
shuang <shuang.lixx@qq.com>
2023-07-08 12:10:16 +0800

    完成searchView页

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import SearchView from '../views/SearchView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -8,7 +8,15 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
-    }
+    },
+    {
+      path: '/search',
+      component: SearchView
+    },
+    // {
+    //   path: '/library',
+    //   component: LibraryView
+    // }
   ]
 })
 
